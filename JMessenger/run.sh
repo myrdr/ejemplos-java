0e1f68b2795570124b27e328ec41ab3d048780c2
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2014-05-12 19:56:56 -0500

    run.sh added

---
+++ JMessenger/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PROGRAMA=edu.itszapopan.jmessenger.Messenger
+
+# Ejecutar el programa
+java $PROGRAMA
+
+#Hacer el archivo Jar
+#jar cfe JMessenger.jar %PROGRAMA% img edu/itszapopan/jmessenger/*.class
+
