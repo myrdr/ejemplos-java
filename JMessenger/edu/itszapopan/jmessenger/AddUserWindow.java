1f83ab5a0d903b10f6467a3090b2b43aa498a9da
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2014-05-19 21:50:09 -0500

    Modificando el dialogo de usuario nuevo

--- JMessenger/edu/itszapopan/jmessenger/AddUserWindow.java
+++ JMessenger/edu/itszapopan/jmessenger/AddUserWindow.java
@@ -8,26 +8,36 @@ public class AddUserWindow extends JDialog {
   private JTextField txtNombre;
   private JTextField txtImagen;
   private JTextField txtIP;
+  private JButton    btnOK, btnCancel;
 
   public AddUserWindow (Frame owner) {
     super(owner,"Agregar Usuarios", true);
 
-    this.setLayout( new GridLayout(5,2,10,10) );
+    this.setLayout( new GridLayout(7,2,10,10) );
+    JLabel space = new JLabel();
 
     txtNombre = new JTextField();
     txtImagen = new JTextField();
     txtIP     = new JTextField();
 
-    add(new JLabel(""));
-    add(new JLabel(""));
+    btnOK = new JButton("Aceptar");
+    btnCancel = new JButton("Cancelar");
+
+    add(space); add(space);
+
     add(new JLabel("Nombre: "));
     add(txtNombre);
+
     add(new JLabel("Imagen: "));
     add(txtImagen);
+
     add(new JLabel("Dirección IP: "));
     add(txtIP);
-    add(new JLabel(""));
-    add(new JLabel(""));
+
+    add(space); add(space);
+
+    add(btnOK);
+    add(btnCancel);
 
     pack();
     setVisible(true);
