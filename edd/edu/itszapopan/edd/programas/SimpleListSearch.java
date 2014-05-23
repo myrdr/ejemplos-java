7f85ed9f743ef2bd310f9ba43ba6f3f7ba6c124c
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2014-05-22 21:09:49 -0500

    Fixing some errors

--- edd/edu/itszapopan/edd/programas/SimpleListSearch.java
+++ edd/edu/itszapopan/edd/programas/SimpleListSearch.java
@@ -8,7 +8,7 @@ public class SimpleListSearch {
    public static void main(String []args) {
       LinkedList ll;
       Scanner teclado;
-      int numero;
+      float numero;
 
       teclado = new Scanner(System.in);
       ll = new LinkedList();
@@ -18,7 +18,7 @@ public class SimpleListSearch {
 
       do  {
         System.out.print("Number: ");
-	numero = teclado.nextInt();
+	numero = teclado.nextFloat();
 
 	if (numero != 0) {
 	  ll.insertIni(numero);
@@ -36,12 +36,12 @@ public class SimpleListSearch {
       System.out.println();
 
       System.out.print("Please type a number: ");
-      numero = teclado.nextInt();
+      numero = teclado.nextFloat();
 
       if ( ll.find(numero) ) {
         System.out.println("The number was found.");
       } else {
-        System.out.println("The "+ number +" was NOT found.");
+        System.out.println("The "+ numero +" was NOT found.");
       }
 
    }
