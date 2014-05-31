bde719b6ab322df3d55a584f2c1c7fc8e861a275
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2014-05-30 21:11:24 -0500

    Ejemplo de borrar un elemento de la lista

---
+++ edd/edu/itszapopan/edd/programas/SimpleListDelete.java
@@ -0,0 +1,55 @@
+package edu.itszapopan.edd.programas;
+
+import edu.itszapopan.edd.listas.*;
+import java.util.Scanner;
+
+public class SimpleListSearch {
+
+   public static void main(String []args) {
+      LinkedList ll;
+      Scanner teclado;
+      int numero;
+
+      teclado = new Scanner(System.in);
+      ll = new LinkedList();
+
+      System.out.println("Linked List Program\n");
+      System.out.println("Please type any number you want (0 to finish).");
+
+      do  {
+        System.out.print("Number: ");
+	numero = teclado.nextInt();
+
+	if (numero != 0) {
+	  ll.insertIni(numero);
+	} 
+
+      } while(numero != 0);
+
+      System.out.println("\n******");
+      System.out.print("The list is: ");
+
+      ll.rewind();
+      while( ll.next() ) {
+	 System.out.print( ll.get() + ", ");
+      }
+      System.out.println();
+
+      System.out.print("Please type a number to erase: ");
+      numero = teclado.nextInt();
+
+      ll.delete(numero);
+
+      System.out.println("\n******");
+      System.out.print("The list is: ");
+
+      ll.rewind();
+      while( ll.next() ) {
+	 System.out.print( ll.get() + ", ");
+      }
+      System.out.println();
+
+   }
+
+}
+
