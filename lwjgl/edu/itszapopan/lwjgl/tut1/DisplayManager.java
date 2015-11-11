b49282564bfd2e70526491633916d3e2c9461b9a
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-11-10 19:34:00 -0600

    add Display Manager file

---
+++ lwjgl/edu/itszapopan/lwjgl/tut1/DisplayManager.java
@@ -0,0 +1,11 @@
+package edu.itszapopan.lwjgl.tut1;
+
+public class DisplayManager {
+
+	public static void createDisplay() {
+	}
+	public static void updateDisplay() {
+	}
+	public static void closeDisplay() {
+	}
+}
