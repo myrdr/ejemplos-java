6cd3a3d8bf5482899cf77fcfc570994a2b8bb675
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-11-10 19:22:14 -0600

    nuevo proyecto de lwjgl

---
+++ lwjgl/readme.md
@@ -0,0 +1,5 @@
+Ejemplos tomados de "Beginners OpenGL 3D Game Tutorials"
+
+
+https://www.youtube.com/playlist?list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP
+
---
+++ lwjgl/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PROGRAMA=edu.itszapopan.edd.programas.SimpleListSearch
+
+# Ejecutar el programa
+java $PROGRAMA
+
+#Hacer el archivo Jar
+#jar cfe JMessenger.jar %PROGRAMA% img edu/itszapopan/jmessenger/*.class
+
